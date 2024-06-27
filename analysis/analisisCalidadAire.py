146d5082624fcedd603b860f5a0715973801493f
Israel Alexis Alzate Tabares <iaalzate@sura.com.co>
2024-06-27 15:42:56 -0500

    crando analisis para power bi

--- analysis/analisisCalidadAire.py
+++ analysis/analisisCalidadAire.py
@@ -8,12 +8,12 @@ def construirDataICA():
     datosICA=generarDatosICA()
     dataFrameICA=pd.DataFrame(datosICA,columns=['comuna','ICA','fecha','ID'])
     dataFrameICA.to_csv('datosica.csv',index=False)
-    #print(dataFrameICA)
+    print(dataFrameICA)
     
     #generando grafica de los datos por comuna
-    datosOrdenadosPorComuna=dataFrameICA.groupby('comuna')['ICA'].mean()
-    plt.figure(figsize=(20,20))
-    datosOrdenadosPorComuna.plot(kind='bar',color='green')
-    plt.show()
+   # datosOrdenadosPorComuna=dataFrameICA.groupby('comuna')['ICA'].mean()
+   # plt.figure(figsize=(20,20))
+   # datosOrdenadosPorComuna.plot(kind='bar',color='green')
+   # plt.show()
 
 construirDataICA()
--- generators/generadorICA.py
+++ generators/generadorICA.py
@@ -4,7 +4,7 @@ def generarDatosICA():
     datosEncuesta=[]
     for i in range(2000):
         dato={}
-        comuna=random.choice(['comuna 1','comuna 2','comuna 3','comuna 4','comuna 5','comuna 6','comuna 7','comuna 8','comuna 9','comuna 10','comuna 11','comuna 12','comuna 13','comuna 14','comuna 15','comuna 16'])
+        comuna=random.choice(['poblado:comuna 1','aranjuez:comuna 2','robledo:comuna 3','manrique:comuna 4','boston:comuna 5','estadio:comuna 6','candelaria:comuna 7','enciso:comuna 8','salvador:comuna 9','barrio trsite:comuna 10','laureles:comuna 11','floresta:comuna 12','san javier:comuna 13','chagualo:comuna 14','castilla:comuna 15','guayabal:comuna 16','sin','-','nan'])
         ica=random.randint(10,100)
         fecha=random.choice(['2024-06-23','2024-06-25','2024-01-30','2024-07-31'])  
         id=random.randint(1,50000)
